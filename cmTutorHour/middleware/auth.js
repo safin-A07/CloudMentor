3e5685327edd4054c92b269e6628adaf8e0d639b
ju4700 <jalal.dev.design@gmail.com>
2025-04-08 21:13:30 +0600

    changes

--- cmTutorHour/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-exports.isAuthenticated = (req, res, next) => {
-    if (!req.session.user) return res.redirect('/auth/login');
-    next();
-};
-
-exports.checkRole = (role) => (req, res, next) => {
-    if (req.session.user.role !== role) return res.status(403).send('Access denied');
-    next();
-};
