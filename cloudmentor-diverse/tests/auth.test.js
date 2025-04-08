d4b13d334ec1dd7833ea40227b9b905255ae7741
ju4700 <jalal.dev.design@gmail.com>
2025-04-08 21:10:45 +0600

    one change

--- cloudmentor-diverse/tests/auth.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../app');
-
-describe('Auth Routes', () => {
-    it('should render login page', async () => {
-        const res = await request(app).get('/auth/login');
-        expect(res.statusCode).toBe(200);
-    });
-});
